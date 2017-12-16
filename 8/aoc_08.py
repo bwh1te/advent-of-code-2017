401ce2053b8a573b644e470a3bec536f28fdd088
bwh1te <aka.bwhite@gmail.com>
2017-12-16 21:01:34 +0700

    Day 8 solution, part 1

---
+++ 8/aoc_08.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from collections import defaultdict
+
+
+def do_instructions(instructions):
+    registers = defaultdict(int)
+    for line in instructions:
+        instr, _, cond = line.partition(" if ")
+        check_reg, _, cond = cond.partition(" ")
+
+        if eval("registers[check_reg] " + cond):
+            reg_name, action, value_str = instr.split(" ")
+            adder = 1 if action == "inc" else -1
+            registers[reg_name] += adder * int(value_str)
+
+    return registers
+
+
+def get_max(registers):
+    max_reg = max(registers, key=registers.get)
+    return max_reg, registers[max_reg]
+
+
+test_input = """b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10""".strip().split("\n")
+reg, val = get_max(do_instructions(test_input))
+assert val == 1
+
+with open("input.txt") as f:
+    task_input = f.read().strip().split("\n")
+
+reg, val = get_max(do_instructions(test_input))
+print("Part 1 answer is {}".format(val))
